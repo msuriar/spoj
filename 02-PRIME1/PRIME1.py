a355b5826ef795948ab7a5098be6cb066663739f
Murali Suriar <code@suriar.com>
2011-12-04 02:10:38 +0000

    Removed pre-computation.

--- 02-PRIME1/PRIME1.py
+++ 02-PRIME1/PRIME1.py
@@ -15,17 +15,16 @@ def main():
     if x+1 != records:
       print ''
 
-
 class Prime(object):
-  def __init__(self, upper_limit=1000000000):
+  def __init__(self):
     self.prime = {}
-    self.upper_limit = upper_limit
-    self.factor_limit = int(math.ceil(math.sqrt(upper_limit)))
-    self._populate_filter_primes()
+    self.filter_primes = [2,3,5,7]
+    self.max_prime = self.filter_primes[-1]
+    for n in self.filter_primes:
+      self._insert_number(n)
 
-  def _populate_filter_primes(self):
-    self.filter_primes = self._primes(self.factor_limit)
-    self.last_filter_prime = self.filter_primes[-1]
+  def max_filter_prime(self):
+    return self.filter_primes[-1]
 
   @staticmethod
   def _primes(n):
@@ -36,7 +35,16 @@ class Prime(object):
       idx = data.index(current) + 1
       data = data[:idx] + [x for x in data[idx:] if x % current != 0]
       current = data[idx]
-    return tuple(data)
+    return data
+
+  def extend_filter_primes(self, new_limit):
+    current = self.filter_primes[-1] + 2
+    data = self.filter_primes + range(current, new_limit)
+    while current < data[-1]:
+      idx = data.index(current) + 1
+      data = data[:idx] + [x for x in data[idx:] if x % current != 0]
+      current = data[idx]
+    self.filter_primes = data
 
   def isprime(self, n):
     if n in self.prime:
@@ -54,24 +62,10 @@ class Prime(object):
     self.prime[n] = is_prime
 
   def gen_output(self, start, stop):
-    if stop <= self.factor_limit:
-      return self._gen_static_output(start, stop)
-    elif start < self.last_filter_prime:
-      first = self._gen_static_output(start,stop)
-      candidates = xrange(self.last_filter_prime+2, stop+1, 2)
-      second = self._gen_upper_output(candidates)
-      return '\n'.join((first, second))
-    elif self.last_filter_prime < start:
-      if start % 2 == 0: start = start+1
-      candidates = xrange(start, stop+1, 2)
-      return self._gen_upper_output(candidates)
-    else:
-      raise Exception("Should not have got here!")
-
-  def _gen_static_output(self, start, stop):
-    return '\n'.join([str(x) for x in self.filter_primes if start <= x <= stop])
-
-  def _gen_upper_output(self, candidates):
+    filter_upper_limit = int(math.ceil(math.sqrt(stop)))
+    if filter_upper_limit > self.filter_primes[-1]:
+      self.extend_filter_primes(filter_upper_limit)
+    candidates = xrange(start, stop+1)
     return '\n'.join([str(x) for x in candidates if self.isprime(x)])
 
 if __name__ == "__main__":
